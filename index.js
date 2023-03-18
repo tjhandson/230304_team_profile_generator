39eb863179989131fd23eec82f5f42501d9e1443
tjhandson <73173000+tjhandson@users.noreply.github.com>
2023-03-18 12:55:54 +0000

    adding Employee code to js file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
+const Engineer = imprequireort("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = import("inquirer");
 const path = require("path");
